71a062a17f0516153865c6198fd69f923fb15480
Matias <amatiascastillo@gmail.com>
2022-07-01 08:30:52 -0300

    refix deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Home from "./pages/home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/inscripciones" element={<Home form="true" />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/inscripciones" element={<Home form="true" />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
