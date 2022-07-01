d6112179af298c3149974fd0afb63cb0767a3cdf
Matias <amatiascastillo@gmail.com>
2022-07-01 08:23:02 -0300

    fix deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/inscripciones" element={<Home form="true" />} />
         <Route path="/" element={<Home />} />
